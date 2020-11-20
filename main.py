69e29d10037a5072134e9420771164e7955a2e74
Christopher Lovejoy <christopher.lovejoy1@gmail.com>
2020-11-20 15:06:47 +0000

    enable command line search rather than config file

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ and print results to the console.
 import yaml
 import video_finder as vf
 
+import argparse
+parser = argparse.ArgumentParser(description='Defining search parameters')
+parser.add_argument('search_terms', type=str, nargs='+',
+                        help='The terms to query. Can be multiple.')
+parser.add_argument('--search_period', type=int, default=7,
+                        help='The number of days to search for.')
+args = parser.parse_args()
+
+
 def load_yaml(filepath):
     """Import YAML config file."""
     with open(filepath, 'r') as stream:
@@ -16,6 +25,6 @@ def load_yaml(filepath):
 config = load_yaml('./config.yaml')
 
 if __name__ == "__main__":
-    start_date_string = vf.get_start_date_string(config['search_period_days'])
-    vf.search_each_term(config['search_terms'], config['api_key'],\
+    start_date_string = vf.get_start_date_string(args.search_period)
+    vf.search_each_term(args.search_terms, config['api_key'],\
                             start_date_string)
