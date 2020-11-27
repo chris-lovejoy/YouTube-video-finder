e934463541151d57cb294be88448ee468f55cf81
dravida <dravidauchkalaranga@gmail.com>
2020-11-26 23:13:12 -0800

    Clean up in prep for alternate execution input entrypoints

--- main.py
+++ main.py
@@ -7,13 +7,6 @@ import yaml
 import video_finder as vf
 import argparse
 
-parser = argparse.ArgumentParser(description='Defining search parameters')
-parser.add_argument('search_terms', type=str, nargs='+',
-                    help='The terms to query. Can be multiple.')
-parser.add_argument('--search-period', type=int, default=7,
-                    help='The number of days to search for.')
-args = parser.parse_args()
-
 
 def load_yaml(filepath):
     """Import YAML config file."""
@@ -22,9 +15,23 @@ def load_yaml(filepath):
             return yaml.safe_load(stream)
         except yaml.YAMLError as exc:
             print(exc)
-config = load_yaml('./config_ext.yaml')
+
+def main(search_terms, search_period, api_key):
+
+    start_date_string = vf.get_start_date_string(search_period)
+    vf.search_each_term(search_terms, api_key, start_date_string)
+
+    return
 
 if __name__ == "__main__":
-    start_date_string = vf.get_start_date_string(args.search_period)
-    vf.search_each_term(args.search_terms, config['api_key'],
-                        start_date_string)
+
+    parser = argparse.ArgumentParser(description='Defining search parameters')
+    parser.add_argument('search_terms', type=str, nargs='+',
+                        help='The terms to query. Can be multiple.')
+    parser.add_argument('--search-period', type=int, default=7,
+                        help='The number of days to search for.')
+    args = parser.parse_args()
+
+    config = load_yaml('./config_ext.yaml')
+
+    main(args.search_terms, args.search_period, config['api_key'])
